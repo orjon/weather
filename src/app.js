5fa5042d8985a3588094cb6af74bc999b058d71c
orjon <github@orjon.com>
2021-02-01 17:53:17 +0000

    Prettier

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import Map from './components/map'
-import SidePanel from './components/sidepanel'
-import './scss/style.scss'
+import Map from './components/map';
+import SidePanel from './components/sidepanel';
+import './scss/style.scss';
 
-const openweatherToken = process.env.REACT_APP_OPEN_WEATHER_TOKEN
+const openweatherToken = process.env.REACT_APP_OPEN_WEATHER_TOKEN;
 
 class App extends Component {
   constructor() {
-    super()
-    this.state = { weatherData: [] }
-    this.mapCenter = { lat: -0.15, lng: 51.51}
-    this.world = ['-3.5,50.5,1.5,55.5,']
-    this.zoomLevel = 25
-    this.handleIconClick = this.handleIconClick.bind(this)
+    super();
+    this.state = { weatherData: [] };
+    this.mapCenter = { lat: -0.15, lng: 51.51 };
+    this.world = ['-3.5,50.5,1.5,55.5,'];
+    this.zoomLevel = 25;
+    this.handleIconClick = this.handleIconClick.bind(this);
   }
 
   componentDidMount() {
-    this.globalWeatherInfo()
+    this.globalWeatherInfo();
     // navigator.geolocation.getCurrentPosition(pos => {
     //   const userPosition = { lat: pos.coords.latitude, lng: pos.coords.longitude}
     //   this.setState({ userPosition })
@@ -25,13 +25,16 @@ class App extends Component {
   }
 
   handleIconClick(id) {
-    this.setState({ clickedLocation: id })
+    this.setState({ clickedLocation: id });
   }
 
   globalWeatherInfo() {
-    axios.get(`https://api.openweathermap.org/data/2.5/box/city?bbox=${this.world}${this.zoomLevel}&APPID=${openweatherToken}`)
-      .then(response => {
-        const tempArray = response.data.list.map(eachLocation => ({
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/box/city?bbox=${this.world}${this.zoomLevel}&APPID=${openweatherToken}`
+      )
+      .then((response) => {
+        const tempArray = response.data.list.map((eachLocation) => ({
           id: eachLocation.id,
           name: eachLocation.name,
           latlng: [eachLocation.coord.Lat, eachLocation.coord.Lon],
@@ -44,24 +47,24 @@ class App extends Component {
           weatherId: eachLocation.weather[0].id,
           weather: eachLocation.weather[0].description,
           weatherClouds: eachLocation.clouds.today,
-          weatherIcon: eachLocation.weather[0].icon
-        }))
-        const weatherData = [...this.state.weatherData, ...tempArray]
-        this.setState({ weatherData })
-      })
+          weatherIcon: eachLocation.weather[0].icon,
+        }));
+        const weatherData = [...this.state.weatherData, ...tempArray];
+        this.setState({ weatherData });
+      });
   }
 
   render() {
     return (
       <main>
-        {this.state.weatherData &&
-        <Map
-          weatherPoints={this.state.weatherData}
-          center={this.mapCenter}
-          points={this.state.weatherData}
-          handleIconClick={this.handleIconClick}
-        />
-        }
+        {this.state.weatherData && (
+          <Map
+            weatherPoints={this.state.weatherData}
+            center={this.mapCenter}
+            points={this.state.weatherData}
+            handleIconClick={this.handleIconClick}
+          />
+        )}
         <SidePanel
           clickedLocation={this.state.clickedLocation}
           weatherData={this.state.weatherData}
@@ -69,7 +72,6 @@ class App extends Component {
       </main>
     );
   }
-
 }
 
 export default App;
--- src/components/sidepanel.js
+++ src/components/sidepanel.js
@@ -1,102 +1,120 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class SidePanel extends Component {
   constructor() {
-    super()
-    this.locale={
-      name: ''
-    }
+    super();
+    this.locale = {
+      name: '',
+    };
   }
 
-  findLocation(locationId)  {
-    this.locale = this.props.weatherData.filter(x => x.id === parseInt(locationId))[0]
+  findLocation(locationId) {
+    this.locale = this.props.weatherData.filter(
+      (x) => x.id === parseInt(locationId)
+    )[0];
     switch (this.locale.weatherIcon) {
       case '01d':
-        this.locale.weatherIcon='day-amin.svg'
-        break
+        this.locale.weatherIcon = 'day-amin.svg';
+        break;
       case '01n':
-        this.locale.weatherIcon='night-amin.svg'
-        break
+        this.locale.weatherIcon = 'night-amin.svg';
+        break;
       case '02d':
-        this.locale.weatherIcon='cloudy-day-1-amin.svg'
-        break
+        this.locale.weatherIcon = 'cloudy-day-1-amin.svg';
+        break;
       case '02n':
-        this.locale.weatherIcon='cloudy-night-1-amin.svg'
-        break
+        this.locale.weatherIcon = 'cloudy-night-1-amin.svg';
+        break;
       case '03d':
-        this.locale.weatherIcon='cloudy-day-3-amin.svg'
-        break
+        this.locale.weatherIcon = 'cloudy-day-3-amin.svg';
+        break;
       case '03n':
-        this.locale.weatherIcon='cloudy-night-3-amin.svg'
-        break
+        this.locale.weatherIcon = 'cloudy-night-3-amin.svg';
+        break;
       case '04d':
-        this.locale.weatherIcon='cloudy-amin.svg'
-        break
+        this.locale.weatherIcon = 'cloudy-amin.svg';
+        break;
       case '04n':
-        this.locale.weatherIcon='cloudy-amin.svg'
-        break
+        this.locale.weatherIcon = 'cloudy-amin.svg';
+        break;
       case '11d':
-        this.locale.weatherIcon='thunder-amin.svg'
-        break
+        this.locale.weatherIcon = 'thunder-amin.svg';
+        break;
       case '11n':
-        this.locale.weatherIcon='thunder-amin.svg'
-        break
+        this.locale.weatherIcon = 'thunder-amin.svg';
+        break;
       case '13d':
-        this.locale.weatherIcon='snowy-6-amin.svg'
-        break
+        this.locale.weatherIcon = 'snowy-6-amin.svg';
+        break;
       case '13n':
-        this.locale.weatherIcon='snowy-6-amin.svg'
-        break
+        this.locale.weatherIcon = 'snowy-6-amin.svg';
+        break;
       case '09d':
-        this.locale.weatherIcon='rainy-5-amin.svg'
-        break
+        this.locale.weatherIcon = 'rainy-5-amin.svg';
+        break;
       case '09n':
-        this.locale.weatherIcon='rainy-5-amin.svg'
-        break
+        this.locale.weatherIcon = 'rainy-5-amin.svg';
+        break;
       case '10d':
-        this.locale.weatherIcon='rainy-6-amin.svg'
-        break
+        this.locale.weatherIcon = 'rainy-6-amin.svg';
+        break;
       case '10n':
-        this.locale.weatherIcon='rainy-6-amin.svg'
-        break
+        this.locale.weatherIcon = 'rainy-6-amin.svg';
+        break;
       case '50d':
-        this.locale.weatherIcon='cloudy-amin.svg'
-        break
+        this.locale.weatherIcon = 'cloudy-amin.svg';
+        break;
       case '50n':
-        this.locale.weatherIcon='cloudy-amin.svg'
-        break
+        this.locale.weatherIcon = 'cloudy-amin.svg';
+        break;
       default:
-        break
+        break;
     }
     // this.locationName = window.location.id
-    return
-
+    return;
   }
 
-
   render() {
-
-    if(!this.props.clickedLocation) return null
-    return(
+    if (!this.props.clickedLocation) return null;
+    return (
       <aside>
         <div className='weatherInfo'>
           {this.findLocation(this.props.clickedLocation)}
           <div className='place'>{this.locale.name}</div>
-          <img className='weatherIcon' src={require(`../images/${this.locale.weatherIcon}`)} alt='weather icon'/>
-          {this.locale.temp && (<div className='temp'>{this.locale.temp.toFixed(1)}°C</div>)}
-          {this.locale.tempMin && this.locale.tempMax && (<div>{this.locale.tempMin.toFixed(1)}°C / {this.locale.tempMax.toFixed(1)}°C</div>)}
+          <img
+            className='weatherIcon'
+            src={require(`../images/${this.locale.weatherIcon}`)}
+            alt='weather icon'
+          />
+          {this.locale.temp && (
+            <div className='temp'>{this.locale.temp.toFixed(1)}°C</div>
+          )}
+          {this.locale.tempMin && this.locale.tempMax && (
+            <div>
+              {this.locale.tempMin.toFixed(1)}°C /{' '}
+              {this.locale.tempMax.toFixed(1)}°C
+            </div>
+          )}
           <div>&nbsp;</div>
-          {this.locale.humidity && (<div>Humidty: {this.locale.humidity}%</div>)}
+          {this.locale.humidity && <div>Humidty: {this.locale.humidity}%</div>}
           <div>&nbsp;</div>
-          {this.locale.windSpeed && (<div>Windspeed: {this.locale.windSpeed} knots</div>)}
-          {this.locale.windDirection && (<div>Direction: {this.locale.windDirection.toFixed(0)}°</div>)}
+          {this.locale.windSpeed && (
+            <div>Windspeed: {this.locale.windSpeed} knots</div>
+          )}
+          {this.locale.windDirection && (
+            <div>Direction: {this.locale.windDirection.toFixed(0)}°</div>
+          )}
           <div>&nbsp;</div>
           <div>{this.locale.weather}</div>
         </div>
-        <div className='limitations'>Update (2019-06-30): Originally this application displayed global weather data. A change in the OpenWeatherMap API now limits free weather data to 25 square degrees.</div>
+        <div className='limitations'>
+          Update (2019-06-30): Originally this application displayed global
+          weather data. A change in the OpenWeatherMap API now limits free
+          weather data to 25 square degrees.
+        </div>
       </aside>
-    )
+    );
   }
 }
 
-export default SidePanel
+export default SidePanel;
